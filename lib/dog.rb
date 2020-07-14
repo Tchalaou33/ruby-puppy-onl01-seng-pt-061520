ffec5a402ca8584a7a8a334c85a3a7fa0b090754
Andrea Tchalaou <attchalaou@gmail.com>
2020-07-14 10:21:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+
+
+class Dog 
+  
+  attr_accessor :name
+  
+@@all= []
+  
+  def initialize (name)
+    @name = name  #changed from a class varible @@ to and instance varible @
+     @@save = save
+  end
+  
+  def self.all
+    @@all
+  end
+
+def save 
+  @@all << self
+end
+
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+end
+end
+
+
+pluto=Dog.new("Pluto")
+pluto.name 
+  
